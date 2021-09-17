fb0238ed935e90bd00306ac87f121387ce664f03
itdao6 [Intan.Maryam] <p130420026@bussan.co.id>
2021-09-17 17:39:34 +0700

    [UPDATE] change function name

--- getstring.go
+++ getstring.go
@@ -2,9 +2,9 @@ package str
 
 import "strings"
 
-// CutStr is a function to get first or last data. Return result in string.
+// GetStr is a function to get first or last data. Return result in string.
 // Pos means position. -1 to get first data and 1 to get last data.
-func CutStr(data string, pos, strlen int) string {
+func GetStr(data string, pos, strlen int) string {
 	var str string
 	if pos == -1 {
 		str = data[:strlen]
